2c5e48273c1e871ea75aac3c19388e17b75cc671
Masamitsu MURASE <masamitsu.murase@gmail.com>
2017-01-05 23:08:12 +0900

    Fix handling of proxy_addr.

--- lib/ruby_installer_manager/auto_manager.rb
+++ lib/ruby_installer_manager/auto_manager.rb
@@ -16,14 +16,15 @@ module RubyInstallerManager
     extend HttpDownload
 
     def self.create(file_list_url=FILE_LIST_URL, proxy_addr: nil, proxy_port: 80, ca_file: Downloader::CA_FILE)
+      self.new(YAML.load(http_get_data(URI(file_list_url), Downloader::REDIRECTION_LIMIT, proxy_addr: proxy_addr, proxy_port: proxy_port, ca_file: ca_file)),
+        proxy_addr: proxy_addr, proxy_port: proxy_port, ca_file: ca_file)
+    end
+
+    def initialize(data, proxy_addr: nil, proxy_port: 80, ca_file: Downloader::CA_FILE)
+      @data = data
       @proxy_addr = proxy_addr
       @proxy_port = proxy_port
       @ca_file = ca_file
-      self.new(YAML.load(http_get_data(URI(file_list_url), Downloader::REDIRECTION_LIMIT, proxy_addr: proxy_addr, proxy_port: proxy_port, ca_file: ca_file)))
-    end
-
-    def initialize(data)
-      @data = data
     end
 
     def ruby_list
